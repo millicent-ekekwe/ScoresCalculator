b7c4bc83df261cbd60e21235fe376395566569a2
millicent-ekekwe <ekekwemillicent@gmail.com>
2024-05-30 11:44:26 +0100

    CalculateScoreCommit

---
+++ CalculateTotalScore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateScore
+{
+    public class CalculateTotalScore
+    {
+        public static int GetTotalScores(int[] number)
+        {
+            int score = 0;
+
+            foreach (int digit in number)
+            {
+                if (digit % 2 == 0)
+                {
+                    score += 1;
+                    if (digit == 8)
+                    {
+                        score += 5;
+                    }
+                }
+                else
+                {
+                    score += 3;
+                }
+            }
+
+            return score;
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Buffers.Text;
+using System.Drawing;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CalculateScore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input1 = { 1, 2, 3, 4, 5 };
+            int[] input2 = { 15, 25, 35 };
+            int[] input3 = { 8, 8 };
+            Console.WriteLine(CalculateTotalScore.GetTotalScores(input1));
+            Console.WriteLine(CalculateTotalScore.GetTotalScores(input2));
+            Console.WriteLine(CalculateTotalScore.GetTotalScores(input3));
+        }
+
+       
+    }
+}
